57be38345f7838a820adffc06e4322af9e315ad5
Sebas Backer <sebasamayaxd@hotmail.com>
2023-02-12 06:42:42 -0500

    Initial structure, folders and init login component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './../assets/css/App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
