9e8d75a1e17025d661e8ffe1f1d692186e05b5b6
Sebas Backer <sebasamayaxd@hotmail.com>
2023-02-12 07:13:46 -0500

    login authentication with token ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Login } from './login/Login';
 import './../assets/css/App.css';
 
 export const App = () => {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useRef } from "react";
+import { url } from './../Const'
+import Swal from "sweetalert2";
+import './../../assets/css/login.css'
 
 export const Login = () => {
-    
+    const refEmail = useRef(null)
+    const refPassword = useRef(null)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: refEmail.current.value, password: refPassword.current.value })
+        }
+        fetch(`${ url }/login`, requestOptions)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data)
+                if(data.ok){
+                    Swal.fire(
+                        'Done!',
+                        `${ data.msg }`,
+                        'success'
+                    ).then(() => {
+                        localStorage.setItem('token', data.token)
+                        window.location.reload()
+                    })
+                }else{
+                    Swal.fire(
+                        'Ups!!',
+                        'Check the fields',
+                        'error'
+                    )
+                }
+            })
+            .catch((error) => {
+                Swal.fire(
+                    'Ups!!',
+                    `${ error }`,
+                    'error'
+                )
+            }
+            )
+    }
+    return(
+        <div className="container-fluid login__container">
+            <div className="">
+                <h2 className="text-center text-white">Login</h2>
+                <form onSubmit={ handleSubmit } className="form">
+                    <input ref={ refEmail } className="form-control" placeholder="Email" type='email' />
+                    <input ref={ refPassword } className="form-control" placeholder="Password" type='password' />
+                    <button type="submit" className="btn btn-success text-white">Iniciar Sesión</button>
+                </form>
+            </div>
+        </div>
+    )
 }
